d5c00d1dc8b134d4fb2533bd2e8024087fc3f8c9
kyuho <123appp@kyonggi.ac.kr>
2026-02-02 02:04:01 +0900

    ci cd 환경변수 테스트

--- src/main/java/politicConnect/auth/AuthController.java
+++ src/main/java/politicConnect/auth/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
 
+
     private final LocalService localService;
     private final SocialLoginService socialLoginService;
 
@@ -46,7 +47,7 @@ public class AuthController {
                 .header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
                 .body(responseBody);
 
-        //테스터
+        //테스터1
     }
 
     @PostMapping("/reissue")
